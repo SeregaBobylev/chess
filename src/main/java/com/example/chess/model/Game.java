a05a38a6b90c6b18ab7e41d9f63b688ab5dc7bf7
Serega06122000 <bobyliov.seryozha@yandex.ru>
2023-01-03 11:50:39 +0300

    ver2

--- src/main/java/com/example/chess/model/Game.java
+++ src/main/java/com/example/chess/model/Game.java
@@ -1,7 +1,9 @@
 package com.example.chess.model;
 
+import com.example.chess.model.boaed.Board;
+
 public class Game {
     private User whiteUser;
     private User blackUser;
-    private Game game;
+    private Board board;
 }
